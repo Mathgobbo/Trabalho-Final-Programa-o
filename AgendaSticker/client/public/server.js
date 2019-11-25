83ba2b8dfad425be68c63c6b1baa1cf9244ff94c
MatheusGobbo <matgobbo@hotmail.com>
2019-11-25 18:19:22 -0300

    mais alterações em relação ao Fetch API

--- AgendaSticker/client/public/server.js
+++ AgendaSticker/client/public/server.js
@@ -1,10 +1,17 @@
 
-function insertData(titulo, subtitulo, descricao){
+function insertData(){
     const fetchUrl = 'http://localhost/AgendaSticker/server/server.php';
-
     fetch(fetchUrl, {
         method: 'post',
         body: new FormData(document.querySelector('#formInsert'))
-    }).then()
+    }).then((res)=>{
+        return res.json;
+    }).then(response=>{
+        console.log(response)
+    })
 
 }
+
+document.querySelector('#submit').addEventListener('click',()=>{
+    insertData();
+})
